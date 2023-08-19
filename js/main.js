4562a9fa10ab2291abc3be7f7048631541405406
rose.the.lexicon <140659755+rose-the-Lexicon@users.noreply.github.com>
2023-08-19 14:19:57 -0500

    Made function to make grid of divs
    Needed this as base for etch a sketch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
-const newDiv = document.createElement("div");
-newDiv.setAttribute("class", "drawing-div");
+const container = document.getElementById("container");
+
+function startEtch (num) {
+    for(let i = 0; i < num; i++){
+        const newParentDiv = document.createElement("div");
+        newParentDiv.className = "drawing-parent-div";
+
+        for(let i = 0; i < num; i++){
+            console.log("@");
+            const newDiv = document.createElement("div");
+            newDiv.className = "drawing-div";
+            newParentDiv.appendChild(newDiv);
+        }
+        container.appendChild(newParentDiv);
+    }
+}
+startEtch(10);
